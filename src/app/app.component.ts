56a9855dfbc13c89f3681040a785a4e39c5a2dec
sivarajh <hariharan.sivaraj@gmail.com>
2021-12-19 00:11:21 -0500

    1 file modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 
-import { selectBookCollection, selectBooks } from './state/books.selectors';
+import { selectBookCollection, selectBooks } from './state/books.selector';
 import { retrievedBookList, addBook, removeBook } from './state/books.actions';
 import { GoogleBooksService } from './book-list/books.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-})
+}) 
 export class AppComponent {
   books$ = this.store.select(selectBooks);
   bookCollection$ = this.store.select(selectBookCollection);
